b0326843eca548d9703bf9f91c32b5f0580cca79
Halyul <xuhaoyuforever@gmail.com>
2017-04-12 19:59:28 +0800

    add gallery page with youtube, html5, vimeo, dailymotion videos supported

--- source/js/js.js
+++ source/js/js.js
@@ -17978,112 +17978,6 @@ var outdatedBrowser = function(options) {
 
 }));
 
-/*! lg-pager - v1.0.2 - 2017-01-22
-* http://sachinchoolur.github.io/lightGallery
-* Copyright (c) 2017 Sachin N; Licensed GPLv3 */
-
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module unless amdModuleId is set
-    define(['jquery'], function (a0) {
-      return (factory(a0));
-    });
-  } else if (typeof exports === 'object') {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like environments that support module.exports,
-    // like Node.
-    module.exports = factory(require('jquery'));
-  } else {
-    factory(jQuery);
-  }
-}(this, function ($) {
-
-(function() {
-
-    'use strict';
-
-    var defaults = {
-        pager: false
-    };
-
-    var Pager = function(element) {
-
-        this.core = $(element).data('lightGallery');
-
-        this.$el = $(element);
-        this.core.s = $.extend({}, defaults, this.core.s);
-        if (this.core.s.pager && this.core.$items.length > 1) {
-            this.init();
-        }
-
-        return this;
-    };
-
-    Pager.prototype.init = function() {
-        var _this = this;
-        var pagerList = '';
-        var $pagerCont;
-        var $pagerOuter;
-        var timeout;
-
-        _this.core.$outer.find('.lg').append('<div class="lg-pager-outer"></div>');
-
-        if (_this.core.s.dynamic) {
-            for (var i = 0; i < _this.core.s.dynamicEl.length; i++) {
-                pagerList += '<span class="lg-pager-cont"> <span class="lg-pager"></span><div class="lg-pager-thumb-cont"><span class="lg-caret"></span> <img src="' + _this.core.s.dynamicEl[i].thumb + '" /></div></span>';
-            }
-        } else {
-            _this.core.$items.each(function() {
-
-                if (!_this.core.s.exThumbImage) {
-                    pagerList += '<span class="lg-pager-cont"> <span class="lg-pager"></span><div class="lg-pager-thumb-cont"><span class="lg-caret"></span> <img src="' + $(this).find('img').attr('src') + '" /></div></span>';
-                } else {
-                    pagerList += '<span class="lg-pager-cont"> <span class="lg-pager"></span><div class="lg-pager-thumb-cont"><span class="lg-caret"></span> <img src="' + $(this).attr(_this.core.s.exThumbImage) + '" /></div></span>';
-                }
-
-            });
-        }
-
-        $pagerOuter = _this.core.$outer.find('.lg-pager-outer');
-
-        $pagerOuter.html(pagerList);
-
-        $pagerCont = _this.core.$outer.find('.lg-pager-cont');
-        $pagerCont.on('click.lg touchend.lg', function() {
-            var _$this = $(this);
-            _this.core.index = _$this.index();
-            _this.core.slide(_this.core.index, false, true, false);
-        });
-
-        $pagerOuter.on('mouseover.lg', function() {
-            clearTimeout(timeout);
-            $pagerOuter.addClass('lg-pager-hover');
-        });
-
-        $pagerOuter.on('mouseout.lg', function() {
-            timeout = setTimeout(function() {
-                $pagerOuter.removeClass('lg-pager-hover');
-            });
-        });
-
-        _this.core.$el.on('onBeforeSlide.lg.tm', function(e, prevIndex, index) {
-            $pagerCont.removeClass('lg-pager-active');
-            $pagerCont.eq(index).addClass('lg-pager-active');
-        });
-
-    };
-
-    Pager.prototype.destroy = function() {
-
-    };
-
-    $.fn.lightGallery.modules.pager = Pager;
-
-})();
-
-
-}));
-
 /*! lg-zoom - v1.0.4 - 2016-12-20
 * http://sachinchoolur.github.io/lightGallery
 * Copyright (c) 2016 Sachin N; Licensed GPLv3 */
@@ -18753,8 +18647,7 @@ $(".contact-me-button").hover(function() {
  * Light Gallery Autoplay v1.0.4 from Line 17344 to Line 17549
  * Light Gallery Video v1.0.1 from Line 17551 to Line 17862
  * Light Gallery Fullscreen v1.0.1 from Line 17864 to Line 17979
- * Light Gallery Pager v1.0.2 from Line 17981 to Line 18085
- * Light Gallery Zoom v1.0.4 from Line 18087 to Line 18617
- * Light Gallery Hash v1.0.1 from Line 18619 to Line 18711
- * Theme script from Line 18713 to Line 18745
+ * Light Gallery Zoom v1.0.4 from Line 17981 to Line 18511
+ * Light Gallery Hash v1.0.1 from Line 18513 to Line 18605
+ * Theme script from Line 18607 to Line 18639
  */
