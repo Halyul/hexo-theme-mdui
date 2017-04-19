86e29bae508a34744eef5aa4c26fc3b33f25f352
eyhn <cneyhn@gmail.com>
2017-04-19 19:13:02 +0800

    fix(animate): diff post card animate

--- source/js/list.js
+++ source/js/list.js
@@ -6,6 +6,8 @@
 
     var columns = [document.createElement("div"), document.createElement("div"), document.createElement("div")];
 
+    columns[1].classList.add("animate-else");
+
     var level = 4;
 
     var sm = 1;
