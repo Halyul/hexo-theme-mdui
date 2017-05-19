ceb01f73732afeeab54357100f324e486156b7c2
Halyul <xuhaoyuforever@gmail.com>
2017-05-19 20:35:31 +0800

    update(css): modularized

--- release.sh
+++ release.sh
@@ -28,13 +28,24 @@ rm $JS_PLUS_CURRENT $JS_NOW_CURRENT
 rm $FONTS_CURRENT
 
 cd $CSS_FILES
-uglifycss outdatedbrowser.css mdui.custom.css font-awesome.css lightgallery.css lg-transitions.css prism-themes.css plus.css --output ../style-plus-v$DATE.min.css
+uglifycss outdatedbrowser.css mdui.custom.css font-awesome.css lightgallery.css lg-transitions.css prism-themes.css --output external.min.css
+
+uglifycss plus/fonts.css plus/global.css plus/appbar.css plus/drawer.css plus/posts.css plus/archives.css plus/dialogs.css plus/page-friends.css plus/page-search.css plus/page-galleries.css plus/page-gallery.css plus/lg.css plus/animate.css --output plus.min.css
+
+uglifycss external.min.css plus.min.css --output ../style-plus-v$DATE.min.css
+
 cssnano ../style-plus-v$DATE.min.css ../style-plus-v$DATE.min.css
 
-uglifycss outdatedbrowser.css mdui.custom.css font-awesome.css lightgallery.css lg-transitions.css prism-themes.css now.css --output ../style-now-v$DATE.min.css
+uglifycss now/fonts.css now/global.css now/appbar.css now/drawer.css now/index.css --output now.min.css
+
+uglifycss external.min.css now.min.css --output ../style-now-v$DATE.min.css
+
 cssnano ../style-now-v$DATE.min.css ../style-now-v$DATE.min.css
+
 # fonts
 uglifycss fonts.css --output ../fonts-v$DATE.min.css
+
+rm external.min.css plus.min.css now.min.css
 
 cd $JS_FILES
 uglifyjs mdui.custom.js lightgallery.js lg-hash.js lg-zoom.js lg-fullscreen.js lg-autoplay.js smooth-scroll.js es6-promise.js fetch.js plus.js --output ../js-plus-v$DATE.min.js
