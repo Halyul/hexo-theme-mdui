0c6fe1069dd28a8d3a3bbb43969544abccfeca63
Halyul <xuhaoyuforever@gmail.com>
2017-06-15 13:42:51 +0800

    remove: snackbar

--- source/js/files/mdui.custom.js
+++ source/js/files/mdui.custom.js
@@ -2,6 +2,12 @@
  * mdui v0.2.1 (https://mdui.org) - Custom Build
  * Copyright 2016-2017 zdhxiong
  * Licensed under MIT
+ * 
+ * Included modules:          material-icons,typo,headroom,collapse,divider,media,ripple,button,fab,grid,toolbar,appbar,card,subheader,grid_list,list,drawer,dialog,shadow,tooltip,chip,menu
+ * Included primary colors:   amber,blue,blue-grey,brown,cyan,deep-orange,deep-purple,green,grey,indigo,light-blue,light-green,lime,orange,pink,purple,red,teal,yellow
+ * Included accent colors:    amber,blue,cyan,deep-orange,deep-purple,green,indigo,light-blue,light-green,lime,orange,pink,purple,red,teal,yellow
+ * Included color degrees:    50,100,200,300,400,500,600,700,800,900,a100,a200,a400,a700
+ * Included layout:           dark
  */
 /* jshint ignore:start */
 ;(function (window, document, undefined) {
@@ -2907,6 +2913,11 @@
 
       TouchHandler.register(e);
 
+      // Chrome 59 点击滚动条时，会在 document 上触发事件
+      if (e.target === document) {
+        return;
+      }
+
       var $ripple;
       var $target = $(e.target);
 
@@ -3061,7 +3072,9 @@
           15 * (_this.$dialBtns.length - index) + 'ms';
       });
 
-      _this.$dial.addClass('mdui-fab-dial-show');
+      _this.$dial
+        .css('height', 'auto')
+        .addClass('mdui-fab-dial-show');
 
       // 如果按钮中存在 .mdui-fab-opened 的图标，则进行图标切换
       if (_this.$btn.find('.mdui-fab-opened').length) {
@@ -3105,6 +3118,7 @@
         if (!_this.$btn.hasClass('mdui-fab-opened')) {
           _this.state = 'closed';
           componentEvent('closed', 'fab', _this, _this.$fab);
+          _this.$dial.css('height', 0);
         }
       });
     };
@@ -4506,222 +4520,6 @@
       }
     });
   });
-
-
-  /**
-   * =============================================================================
-   * ************   Snackbar   ************
-   * =============================================================================
-   */
-
-  (function () {
-
-    /**
-     * 当前打开着的 Snackbar
-     */
-    var currentInst;
-
-    /**
-     * 对列名
-     * @type {string}
-     */
-    var queueName = '__md_snackbar';
-
-    var DEFAULT = {
-      message: '',                    // 文本内容
-      timeout: 4000,                  // 在用户没有操作时多长时间自动隐藏
-      buttonText: '',                 // 按钮的文本
-      buttonColor: '',                // 按钮的颜色，支持 blue #90caf9 rgba(...)
-      closeOnButtonClick: true,       // 点击按钮时关闭
-      closeOnOutsideClick: true,      // 触摸或点击屏幕其他地方时关闭
-      onClick: function () {          // 在 Snackbar 上点击的回调
-      },
-
-      onButtonClick: function () {    // 点击按钮的回调
-      },
-
-      onClose: function () {          // 关闭动画开始时的回调
-      },
-    };
-
-    /**
-     * 点击 Snackbar 外面的区域关闭
-     * @param e
-     */
-    var closeOnOutsideClick = function (e) {
-      var $target = $(e.target);
-      if (!$target.hasClass('mdui-snackbar') && !$target.parents('.mdui-snackbar').length) {
-        currentInst.close();
-      }
-    };
-
-    /**
-     * Snackbar 实例
-     * @param opts
-     * @constructor
-     */
-    function Snackbar(opts) {
-      var _this = this;
-
-      _this.options = $.extend({}, DEFAULT, (opts || {}));
-
-      // message 参数必须
-      if (!_this.options.message) {
-        return;
-      }
-
-      _this.state = 'closed';
-
-      _this.timeoutId = false;
-
-      // 按钮颜色
-      var buttonColorStyle = '';
-      var buttonColorClass = '';
-
-      if (
-        _this.options.buttonColor.indexOf('#') === 0 ||
-        _this.options.buttonColor.indexOf('rgb') === 0
-      ) {
-        buttonColorStyle = 'style="color:' + _this.options.buttonColor + '"';
-      } else if (_this.options.buttonColor !== '') {
-        buttonColorClass = 'mdui-text-color-' + _this.options.buttonColor;
-      }
-
-      // 添加 HTML
-      _this.$snackbar = $(
-        '<div class="mdui-snackbar">' +
-          '<div class="mdui-snackbar-text">' +
-            _this.options.message +
-          '</div>' +
-          (_this.options.buttonText ?
-            ('<a href="javascript:void(0)" ' +
-            'class="mdui-snackbar-action mdui-btn mdui-ripple mdui-ripple-white ' +
-              buttonColorClass + '" ' +
-              buttonColorStyle + '>' +
-              _this.options.buttonText +
-            '</a>') :
-            ''
-          ) +
-        '</div>')
-        .appendTo(document.body);
-
-      // 设置位置
-      _this.$snackbar
-        .transform('translateY(' + _this.$snackbar[0].clientHeight + 'px)')
-        .css('left', (document.body.clientWidth - _this.$snackbar[0].clientWidth) / 2 + 'px')
-        .addClass('mdui-snackbar-transition');
-    }
-
-    /**
-     * 打开 Snackbar
-     */
-    Snackbar.prototype.open = function () {
-      var _this = this;
-
-      if (_this.state === 'opening' || _this.state === 'opened') {
-        return;
-      }
-
-      // 如果当前有正在显示的 Snackbar，则先加入队列，等旧 Snackbar 关闭后再打开
-      if (currentInst) {
-        queue.queue(queueName, function () {
-          _this.open();
-        });
-
-        return;
-      }
-
-      currentInst = _this;
-
-      // 开始打开
-      _this.state = 'opening';
-      _this.$snackbar
-        .transform('translateY(0)')
-        .transitionEnd(function () {
-          if (_this.state !== 'opening') {
-            return;
-          }
-
-          _this.state = 'opened';
-
-          // 有按钮时绑定事件
-          if (_this.options.buttonText) {
-            _this.$snackbar
-              .find('.mdui-snackbar-action')
-              .on('click', function () {
-                _this.options.onButtonClick();
-                if (_this.options.closeOnButtonClick) {
-                  _this.close();
-                }
-              });
-          }
-
-          // 点击 snackbar 的事件
-          _this.$snackbar.on('click', function (e) {
-            if (!$(e.target).hasClass('mdui-snackbar-action')) {
-              _this.options.onClick();
-            }
-          });
-
-          // 点击 Snackbar 外面的区域关闭
-          if (_this.options.closeOnOutsideClick) {
-            $document.on(TouchHandler.start, closeOnOutsideClick);
-          }
-
-          // 超时后自动关闭
-          _this.timeoutId = setTimeout(function () {
-            _this.close();
-          }, _this.options.timeout);
-        });
-    };
-
-    /**
-     * 关闭 Snackbar
-     */
-    Snackbar.prototype.close = function () {
-      var _this = this;
-
-      if (_this.state === 'closing' || _this.state === 'closed') {
-        return;
-      }
-
-      if (_this.timeoutId) {
-        clearTimeout(_this.timeoutId);
-      }
-
-      if (_this.options.closeOnOutsideClick) {
-        $document.off(TouchHandler.start, closeOnOutsideClick);
-      }
-
-      _this.state = 'closing';
-      _this.options.onClose();
-
-      _this.$snackbar
-        .transform('translateY(' + _this.$snackbar[0].clientHeight + 'px)')
-        .transitionEnd(function () {
-          if (_this.state !== 'closing') {
-            return;
-          }
-
-          currentInst = null;
-          _this.state = 'closed';
-          _this.$snackbar.remove();
-          queue.dequeue(queueName);
-        });
-    };
-
-    /**
-     * 打开 Snackbar
-     * @param params
-     */
-    mdui.snackbar = function (params) {
-      var inst = new Snackbar(params);
-
-      inst.open();
-      return inst;
-    };
-
-  })();
 
 
   /**
