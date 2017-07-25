ad4ac146345754435b1a8cfc31c19479bb6089ca
Halyul <xuhaoyuforever@gmail.com>
2017-07-25 21:15:57 +0800

    chore(ls): update script

--- source/js/_files/js.js
+++ source/js/_files/js.js
@@ -1,8 +1,6 @@
 /*! Copyright 2017 Halyul */
 console.info("%cCopyright © 2017 Halyul\n"+'Theme Version: 1.4.5'+'%c\nhttps://github.com/Halyul/hexo-theme-mdui',"font-size: 14px;color: #3F51B5;","color: #448AFF;text-decoration: none");
 
-var $$ = mdui.JQ;
-
 /* smooth scroll */
 (function() {
   if (document.readyState === 'complete' || document.readyState !== 'loading') {
