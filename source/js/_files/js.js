5a4d970eff9277089ca855c00bcd7c1b2d261ffe
Halyul <xuhaoyuforever@gmail.com>
2017-07-25 21:18:38 +0800

    fix(ls): main_js always gets reloaded

--- source/js/_files/js.js
+++ source/js/_files/js.js
@@ -1,6 +1,3 @@
-/*! Copyright 2017 Halyul */
-console.info("%cCopyright © 2017 Halyul\n"+'Theme Version: 1.4.5'+'%c\nhttps://github.com/Halyul/hexo-theme-mdui',"font-size: 14px;color: #3F51B5;","color: #448AFF;text-decoration: none");
-
 /* smooth scroll */
 (function() {
   if (document.readyState === 'complete' || document.readyState !== 'loading') {
