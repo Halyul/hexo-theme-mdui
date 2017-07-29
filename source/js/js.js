179b0a295dda23ddef3f40873aeb5a9da6b0ea90
Halyul <xuhaoyuforever@gmail.com>
2017-07-29 18:48:33 +0800

    feat(refactor/2.0): finish appbar and drawer

---
+++ source/js/js.js
@@ -0,0 +1,64 @@
+var drawer = new mdui.Drawer('#drawer', {swipe: true});
+
+function BarbaSettings() {
+  // Barbajs settings
+  var burger = document.querySelector('.theme-toolbar-burger');
+  var BarbaIndex = Barba.BaseView.extend({
+    namespace: 'index',
+    onEnter: function() {
+      burger.classList.remove('back');
+      burger.addEventListener('click', drawerToggle)
+    },
+    onEnterCompleted: function() {
+
+    },
+    onLeave: function() {
+      burger.removeEventListener('click', drawerToggle)
+      drawer.close();
+    },
+    onLeaveCompleted: function() {
+
+    }
+  });
+
+  // Barbajs settings
+  var BarbaNIndex = Barba.BaseView.extend({
+    namespace: 'NIndex',
+    onEnter: function() {
+      burger.classList.add('back');
+      burger.addEventListener('click', backToPrev)
+    },
+    onEnterCompleted: function() {
+
+    },
+    onLeave: function() {
+      burger.removeEventListener('click', backToPrev)
+    },
+    onLeaveCompleted: function() {
+
+    }
+  });
+
+  // Don't forget to init the view!
+  BarbaIndex.init();
+  BarbaNIndex.init();
+}
+
+// init Barbajs
+function initBarba() {
+  BarbaSettings()
+  Barba.Pjax.start();
+}
+if (document.readyState === 'complete' || document.readyState !== 'loading') {
+  initBarba();
+} else {
+  document.addEventListener('DOMContentLoaded', initBarba);
+}
+
+function backToPrev() {
+  window.history.back()
+}
+
+function drawerToggle() {
+  drawer.toggle()
+}
