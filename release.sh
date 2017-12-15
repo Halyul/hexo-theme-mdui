a2486df4eb4c653c49582dde3a9c5e99869713ac
Halyul <xuhaoyuforever@gmail.com>
2017-12-14 21:40:38 -0500

    feat(release script): update script

--- release.sh
+++ release.sh
@@ -1,73 +1,62 @@
 #!/bin/bash
-CSS=$PWD/source/css
-JS=$PWD/source/js
-
-CSS_PLUS=$(find $CSS/style-plus.min.css)
-CSS_NOW=$(find $CSS/style-now.min.css)
-FONTS=$(find $CSS/fonts.min.css)
-JS=$(find $JS/js.min.js)
-
 CSS_FILES=$PWD/source/css/_files
 JS_FILES=$PWD/source/js/_files
 
-# delete old files
-rm $CSS_PLUS $CSS_NOW
-rm $JS
-rm $FONTS
-
-cd $CSS_FILES
+cd $CSS_FILES/prism
 
 # external core css
-uglifycss core/outdatedbrowser.css core/mdui.custom.css core/font-awesome.css core/animate.css --output external-core.min.css
-
-# plus
-## plus core
-uglifycss plus/global.css plus/appbar.css plus/drawer.css --output plus.min.css
-
-## combine core css
-uglifycss external-core.min.css plus.min.css --output ../style-plus.min.css
-
-cssnano ../style-plus.min.css ../style-plus.min.css --safe
-
-## plus other
-### plus index
-cssnano plus/index.css ../src/plus/index.min.css --safe
-
-### plus post
-cssnano plus/post.css ../src/plus/post.min.css --safe
-
-### plus side content
-cssnano plus/side_content.css ../src/plus/side_content.min.css --safe
-
-### plus search
-cssnano plus/search.css ../src/plus/search.min.css --safe
-
-# now
-## now core
-uglifycss now/global.css now/appbar.css now/drawer.css now/posts.css now/index.css --output now.min.css
-
-## combine core css
-uglifycss external-core.min.css now.min.css --output ../style-now.min.css
-
-cssnano ../style-now.min.css ../style-now.min.css --safe
-
-## now other
-### now post
-cssnano now/post.css ../src/now/post.min.css --safe
-
-### plus side content
-cssnano now/side_content.css ../src/now/side_content.min.css --safe
-
-# shared
-cssnano shared/archives.css ../src/shared/archives.min.css --safe
-cssnano shared/aboutme_dialog.css ../src/shared/aboutme_dialog.min.css --safe
-
-# fonts
-uglifycss fonts.css --output ../fonts.min.css
-
-rm external-core.min.css plus.min.css now.min.css
+A=a11y-dark
+uglifycss $A.css --output ../../prism/$A.min.css
+A=atom-dark
+uglifycss $A.css --output ../../prism/$A.min.css
+A=base16-light
+uglifycss $A.css --output ../../prism/$A.min.css
+A=cb
+uglifycss $A.css --output ../../prism/$A.min.css
+A=coy
+uglifycss $A.css --output ../../prism/$A.min.css
+A=darcula
+uglifycss $A.css --output ../../prism/$A.min.css
+A=dark
+uglifycss $A.css --output ../../prism/$A.min.css
+A=default
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-dark
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-earth
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-forest
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-light
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-sea
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-space
+uglifycss $A.css --output ../../prism/$A.min.css
+A=funky
+uglifycss $A.css --output ../../prism/$A.min.css
+A=ghcolors
+uglifycss $A.css --output ../../prism/$A.min.css
+A=hopscotch
+uglifycss $A.css --output ../../prism/$A.min.css
+A=line-number
+uglifycss $A.css --output ../../prism/$A.min.css
+A=okaidia
+uglifycss $A.css --output ../../prism/$A.min.css
+A=pojoaque
+uglifycss $A.css --output ../../prism/$A.min.css
+A=solarizedlight
+uglifycss $A.css --output ../../prism/$A.min.css
+A=tomorrow
+uglifycss $A.css --output ../../prism/$A.min.css
+A=twilight
+uglifycss $A.css --output ../../prism/$A.min.css
+A=vs
+uglifycss $A.css --output ../../prism/$A.min.css
+A=xonokai
+uglifycss $A.css --output ../../prism/$A.min.css
 
 cd $JS_FILES
-uglifyjs mdui.custom.js smooth-scroll.js es6-promise.js fetch.js js.js --output ../js.min.js
+uglifyjs barba.js es6-promise.js fetch.js mdui.js smooth-scroll.js js.js --output ../js.min.js
 
-uglifyjs lightgallery.js lg-hash.js lg-zoom.js lg-fullscreen.js lg-autoplay.js --output ../lightgallery.min.js
+uglifyjs lightgallery.js lg-zoom.js lg-fullscreen.js lg-autoplay.js --output ../lightgallery.min.js
--- source/js/_files/js.js
+++ source/js/_files/js.js
@@ -54,7 +54,6 @@ function initPages() {
   themeRuntime.init.status = true;
 }
 
-themeRuntime.init = {};
 themeRuntime.init.status = false;
 themeRuntime.init.posts = Barba.BaseView.extend({
   namespace: 'posts',
@@ -65,7 +64,7 @@ themeRuntime.init.posts = Barba.BaseView.extend({
 
   },
   onLeave: function() {
-    
+
   },
   onLeaveCompleted: function() {
 
