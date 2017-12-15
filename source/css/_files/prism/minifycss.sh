2a74909cb16a78c3f0193d3ca086dfeed9a60913
Halyul <xuhaoyuforever@gmail.com>
2017-12-14 20:29:35 -0500

    feat(prism): add back prism

---
+++ source/css/_files/prism/minifycss.sh
@@ -0,0 +1,50 @@
+A=a11y-dark
+uglifycss $A.css --output ../../prism/$A.min.css
+A=atom-dark
+uglifycss $A.css --output ../../prism/$A.min.css
+A=base16-light
+uglifycss $A.css --output ../../prism/$A.min.css
+A=cb
+uglifycss $A.css --output ../../prism/$A.min.css
+A=coy
+uglifycss $A.css --output ../../prism/$A.min.css
+A=darcula
+uglifycss $A.css --output ../../prism/$A.min.css
+A=dark
+uglifycss $A.css --output ../../prism/$A.min.css
+A=default
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-dark
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-earth
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-forest
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-light
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-sea
+uglifycss $A.css --output ../../prism/$A.min.css
+A=duotone-space
+uglifycss $A.css --output ../../prism/$A.min.css
+A=funky
+uglifycss $A.css --output ../../prism/$A.min.css
+A=ghcolors
+uglifycss $A.css --output ../../prism/$A.min.css
+A=hopscotch
+uglifycss $A.css --output ../../prism/$A.min.css
+A=line-number
+uglifycss $A.css --output ../../prism/$A.min.css
+A=okaidia
+uglifycss $A.css --output ../../prism/$A.min.css
+A=pojoaque
+uglifycss $A.css --output ../../prism/$A.min.css
+A=solarizedlight
+uglifycss $A.css --output ../../prism/$A.min.css
+A=tomorrow
+uglifycss $A.css --output ../../prism/$A.min.css
+A=twilight
+uglifycss $A.css --output ../../prism/$A.min.css
+A=vs
+uglifycss $A.css --output ../../prism/$A.min.css
+A=xonokai
+uglifycss $A.css --output ../../prism/$A.min.css
